ce0039c8fd27299afc9aef2bf5bb26a7e125ebd3
daniel <dotexe0@gmail.com>
2018-12-28 12:06:13 -0800

    add state to application

--- App.js
+++ App.js
@@ -1,31 +1,70 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
+import uuidv4 from "uuid/v4";
 import EditableTimer from "./components/EditableTimer";
 import ToggleableTimeForm from "./components/ToggleableTimerForm";
+import { newTimer } from "./utils/TimerUtils";
 
 export default class App extends Component {
+  state = {
+    timers: [
+      {
+        title: "Mow the lawn",
+        project: "House Chores",
+        id: uuidv4(),
+        elapsed: 30000,
+        isRunning: true
+      },
+      {
+        title: "Call Geico",
+        project: "Cleanup Stuff",
+        id: uuidv4(),
+        elapsed: 1536000,
+        isRunning: false
+      }
+    ]
+  };
+
+  handleCreateFormSubmit = timer => {
+    const { timers } = this.state;
+    this.setState({ timers: [newTimer(timer), ...timers] });
+  };
+
+  handleFormSubmit = formData => {
+    const { timers } = this.state;
+    this.setState({
+      timers: timers.map(timer => {
+        if (timer.id === formData.id) {
+          const { title, project } = formData
+          return {
+            ...timer, title, project
+          }
+        }
+        return timer
+      })
+    })
+  }
+
   render() {
+    const { timers } = this.state;
     return (
       <View style={styles.appContainer}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Timers</Text>
         </View>
         <ScrollView style={styles.timerList}>
-          <ToggleableTimeForm isOpen={false} />
-          <EditableTimer
-            id="1"
-            title="Mow the lawn"
-            project="house Chors"
-            elapsed="8938442"
-            isRunning
-          />
-          <EditableTimer
-            id="2"
-            title="Bake sqash"
-            project="Kitchen Chores"
-            elapsed="342424242"
-            editFormOpen
-          />
+          <ToggleableTimeForm onFormSubmit={this.handleCreateFormSubmit} />
+          {timers.map(({ id, title, project, elapsed, isRunning }) => (
+            <EditableTimer
+              key={id}
+              id={id}
+              title={title}
+              project={project}
+              elapsed={elapsed}
+              isRunning={isRunning}
+              onFormSubmit={this.handleFormSubmit}
+            />
+          ))}
         </ScrollView>
       </View>
     );
--- components/EditableTimer.js
+++ components/EditableTimer.js
@@ -1,26 +1,60 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import TimerForm from './TimerForm'
-import Timer from './Timer'
+import TimerForm from "./TimerForm";
+import Timer from "./Timer";
 
-export default function EditableTimer({
-  id,
-  title,
-  project,
-  elapsed,
-  isRunning,
-  editFormOpen }) {
-  if (editFormOpen) {
-    return < TimerForm id={id} title={title} project={project} />
+export default class EditableTimer extends Component {
+  state = {
+    editFormOpen: false
+  };
+
+  handleEditPress = () => {
+    this.openForm();
+  };
+
+  handleFormClose = () => {
+    this.closeForm();
+  };
+
+  handleSubmit = timer => {
+    const { onFormSubmit } = this.props;
+    onFormSubmit(timer);
+    this.closeForm();
+  };
+
+  closeForm = () => {
+    this.setState({ editFormOpen: false });
+  };
+
+  openForm = () => {
+    this.setState({ editFormOpen: true });
+  };
+
+  render() {
+    const { id, title, project, elapsed, isRunning } = this.props;
+    const { editFormOpen } = this.state;
+
+    if (editFormOpen) {
+      return (
+        <TimerForm
+          id={id}
+          title={title}
+          project={project}
+          onFormSubmit={this.handleSubmit}
+          onFormClose={this.handleFormClose}
+        />
+      );
+    }
+
+    return (
+      <Timer
+        id={id}
+        title={title}
+        project={project}
+        elapsed={elapsed}
+        isRunning={isRunning}
+        onEditPress={this.handleEditPress}
+      />
+    );
   }
-  
-  return (
-    <Timer 
-      id={id}
-      title={title}
-      project={project}
-      elapsed={elapsed}
-      isRunning={isRunning}
-    />
-  )
 }
